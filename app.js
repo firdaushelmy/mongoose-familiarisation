bd058cffbdb70f73ce8375bc9565b8968f1da353
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-16 01:44:17 +0800

    add schema, constructor, and use it to add new record

--- app.js
+++ app.js
@@ -1,50 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/fruitsDB');
-// Connection URL
-const url = 'mongodb://localhost:27017';
+mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Database Name
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// Use connect method to connect to the Server
-client.connect(function (err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  findDocuments(db, function () {
-    client.close()
-  });
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
 });
 
-const insertDocuments = function (db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-    {
-      name: "Apple",
-      score: 8,
-      review: "Great fruit!"
-    },
-    {
-      name: "Orange",
-      score: 6,
-      review: "Kinda sour"
-    },
-  ], function (err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  })
-};
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+const fruit = new Fruit({
+  name: 'Apple',
+  rating: 7,
+  review: 'pretty solid for a fruit',
+});
+
+fruit.save()
+
 
 const findDocuments = function (db, callback) {
   // Get the documents collection
